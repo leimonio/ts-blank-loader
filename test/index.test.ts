6039580c6238c86d0614286b6ef22497fc563eda
Konstantinos Leimonis <k.leimonis@gmail.com>
2024-09-30 13:50:40 +0100

    fix: testing for loader

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,27 @@
-import { test, assert } from "vitest"
-import tsBlankLoader from "../src"
+import { afterEach, describe, expect, it, vi } from "vitest"
+import tsBlankLoaderOriginal from "../src"
 
-test("ts-blank-loader", async () => {
-  const result = await tsBlankLoader(`let x: string;`)
-  assert.equal(result, "let x        ;")
+function setupModule() {
+  const mockCallback = vi.fn()
+  const mockThis = {
+    async: vi.fn().mockImplementation(() => mockCallback),
+  }
+  const tsBlankLoader = tsBlankLoaderOriginal.bind(mockThis)
+  return { mockCallback, mockThis, tsBlankLoader }
+}
+
+describe("ts-blank-loader", () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it("should call callback with transpiled content", async () => {
+    const { mockCallback, mockThis, tsBlankLoader } = setupModule()
+
+    await tsBlankLoader(`let x: string;`)
+
+    expect(mockThis.async).toHaveBeenCalledTimes(1)
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+    expect(mockCallback).toHaveBeenCalledWith(null, "let x        ;")
+  })
 })
