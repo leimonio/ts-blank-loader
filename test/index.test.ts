d905ec86893211897d061a0fcaf2c48e7e5073f9
leimonio <1969742+leimonio@users.noreply.github.com>
2024-10-02 06:59:31 +0100

    feat: tsx support (#1)
    * test: add tsx
    
    * feat: add tsx compilation

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,11 @@
 import { afterEach, describe, expect, it, vi } from "vitest"
 import tsBlankLoaderOriginal from "../src"
 
-function setupModule() {
+function setupModule({ mockedThis } = { mockedThis: {} }) {
   const mockCallback = vi.fn()
   const mockThis = {
     async: vi.fn().mockImplementation(() => mockCallback),
+    ...mockedThis,
   }
   const tsBlankLoader = tsBlankLoaderOriginal.bind(mockThis)
   return { mockCallback, mockThis, tsBlankLoader }
@@ -23,5 +24,29 @@ describe("ts-blank-loader", () => {
     expect(mockThis.async).toHaveBeenCalledTimes(1)
     expect(mockCallback).toHaveBeenCalledTimes(1)
     expect(mockCallback).toHaveBeenCalledWith(null, "let x        ;")
+  })
+
+  it("should call callback with preserved tsx", async () => {
+    const { mockCallback, mockThis, tsBlankLoader } = setupModule({
+      mockedThis: {
+        resourcePath: "App.tsx",
+      },
+    })
+
+    const tsxContent = `
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello, World!</h1>
+    </div>
+  )
+}
+`
+
+    await tsBlankLoader(tsxContent)
+
+    expect(mockThis.async).toHaveBeenCalledTimes(1)
+    expect(mockCallback).toHaveBeenCalledTimes(1)
+    expect(mockCallback).toHaveBeenCalledWith(null, tsxContent)
   })
 })
